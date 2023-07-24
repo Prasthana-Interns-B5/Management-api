99b40bcefa08d239bd4d1aea7f1d3de5ba7d7d4e
Alekhya koppu <alekhya.koppu@prasthana.com>
2023-07-24 16:37:43 +0530

    dleted migrations

--- db/migrate/20230521100245_add_column_to_feedbacks.rb
+++
@@ -1,5 +0,0 @@
-class AddColumnToFeedbacks < ActiveRecord::Migration[7.0]
-  def change
-    add_reference :feedbacks, :employee, null: false, foreign_key: true
-  end
-end
--- db/migrate/20230522054935_add_column_one_one_to_feedbacks.rb
+++
@@ -1,5 +0,0 @@
-class AddColumnOneOneToFeedbacks < ActiveRecord::Migration[7.0]
-  def change
-    add_column :feedbacks, :meeting, :boolean , default: false
-  end
-end
