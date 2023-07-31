0f157e88a3b386c44616b6cf5e13fbea70ce0046
Susarla Sri Vardhani <vardhani.susarla@prasthana.com>
2023-07-31 16:53:35 +0530

    notifications

--- app/services/notification_service.rb
+++ app/services/notification_service.rb
@@ -1,7 +1,7 @@
 class NotificationService
     require 'fcm'
   
-    def self.send_notification_to_employees(tokens, title, body)
+    def self.send_notification(tokens, title, body)
         fcm = FCM.new(FCM_SERVER_KEY) 
         options = { notification: { title: title, body: body } }
         tokens.each do |device_token|
