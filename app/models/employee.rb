ca741147fa1263050f1a1ce938fc34a03d26a11b
Satyaprakash0081 <satyaprakash.maharana@prasthana.com>
2024-05-20 03:10:04 +0530

    Initial commit of satya prakash

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,16 +1,6 @@
 class Employee < ApplicationRecord
+  validates :name, :role, presence: true
 
-  include Devise::JWT::RevocationStrategies::JTIMatcher
-  validates :name,:role,presence: true
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-  :recoverable, :rememberable, :validatable,
-  :jwt_authenticatable, jwt_revocation_strategy: self
-
-
-
-  
   has_many :questions
   has_many :answers
   has_many :points
@@ -33,31 +23,6 @@ class Employee < ApplicationRecord
       id = Current.employee.id
       search_name = params[:name]
       employees = Employee.where(reporting_manager_id:id).where("LOWER(name) LIKE :query ", query: "%#{search_name}%")
-    end
-  end
-
-  def jwt_payload
-    super
-  end
-
-  def on_jwt_dispatch(token, payload)
-    @auth_token = token
-  end
-  def auth_token
-    @auth_token
-  end
-  def expiration_time
-    decoded_token = JWT.decode(@auth_token,Rails.application.credentials.fetch(:secret_key_base), true, { algorithm: 'HS256' })
-    payload = decoded_token.first
-    payload['exp']
-  end
-
-  ROLES = %w{ur_hr ur_manager ur_sr_software ur_lead ur_software_engineer}
-  validates :role, inclusion: { in: ROLES,
-  message: "%{value} is not valid." }
-  ROLES.each do |role_name|
-    define_method "#{role_name}?" do
-      role == role_name
     end
   end
 end
