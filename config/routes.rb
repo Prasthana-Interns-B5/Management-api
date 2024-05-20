a4f130b3a14a4a36fe1352fd2f286182b5cb07a1
Chintu sai <chintusai.maddi@prasthana.com>
2024-05-20 09:09:11 +0530

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -16,32 +16,29 @@ Rails.application.routes.draw do
     get "reviews", on: :member
     get "current_employee_info", on: :collection
     get "all_employees", on: :collection
+    resources :one_on_one,only: :index
+    resources :employee_points
   end
-      
+
 
   resources :questions do
     get "replies", on: :member
   end
 
   resources :answers
-  resources :home_page 
+  resources :home_page
   resources :reference_data
 
 
-
-  resources :points do
-    put "manager_score", on: :member
-  end
-
   resources :feedbacks do
-    get "get_individual_feedback", on: :member 
+    get "get_individual_feedback", on: :member
     get "get_individual_feedback_type", on: :collection
     get "get_month_based_records", on: :collection
     get "flag_based_records",on: :collection
   end
 
 
-  resources :one_on_ones do
+  resources :one_on_ones, except: :index do
     get "meetings", on: :collection
     get "reviews", on: :member
     get "rating", on: :member
@@ -62,5 +59,3 @@ Rails.application.routes.draw do
   end
 
 end
-
-
