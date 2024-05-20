6c7d45245c45592e16513ecc34b4a8809607bf33
Satyaprakash0081 <satyaprakash.maharana@prasthana.com>
2024-05-20 12:01:10 +0530

    Initial 2 commit of satya prakash

--- config/routes.rb
+++ config/routes.rb
@@ -5,22 +5,21 @@ Rails.application.routes.draw do
   end
   devise_for :users
   devise_scope :user do
-    post 'generate_otp', to: 'users#generate_otp'
+    post 'generate_and_send_otp', to: 'users#generate_and_send_otp'
     post 'verify_email_auth_code', to: 'users#verify_email_auth_code'
   end
   resources :employees do
-    get "subordinates", on: :collection
-    get "queries", on: :member
-    get "scores", on: :member
-    put "role", on: :member
-    get "reviews", on: :member
-    get "current_employee_info", on: :collection
-    get "all_employees", on: :collection
+    get 'subordinates', on: :collection
+    get 'queries', on: :member
+    get 'scores', on: :member
+    put 'role', on: :member
+    get 'reviews', on: :member
+    get 'current_employee_info', on: :collection
+    get 'all_employees', on: :collection
   end
-      
 
   resources :questions do
-    get "replies", on: :member
+    get 'replies', on: :member
   end
 
   resources :answers
@@ -30,37 +29,33 @@ Rails.application.routes.draw do
 
 
   resources :points do
-    put "manager_score", on: :member
+    put 'manager_score', on: :member
   end
 
   resources :feedbacks do
-    get "get_individual_feedback", on: :member 
-    get "get_individual_feedback_type", on: :collection
-    get "get_month_based_records", on: :collection
-    get "flag_based_records",on: :collection
+    get 'get_individual_feedback', on: :member 
+    get 'get_individual_feedback_type', on: :collection
+    get 'get_month_based_records', on: :collection
+    get 'flag_based_records',on: :collection
   end
 
 
   resources :one_on_ones do
-    get "meetings", on: :collection
-    get "reviews", on: :member
-    get "rating", on: :member
+    get 'meetings', on: :collection
+    get 'reviews', on: :member
+    get 'rating', on: :member
   end
 
   resources :reviews do
     get 'find_by_type', on: :collection
-    get "comments", on: :member
+    get 'comments', on: :member
   end
 
   resources :comments
 
   resources :notifications do
-    get "send_notification", on: :collection
+    get 'send_notification', on: :collection
   end
-  resources :device_tokens do
-    get "find_by_token", on: :collection
-  end
-
 end
 
 
