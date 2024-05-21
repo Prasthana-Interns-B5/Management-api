01559d30a9ea0666056b7184bd40d6102a2095a5
Satyaprakash0081 <satyaprakash.maharana@prasthana.com>
2024-05-21 14:38:50 +0530

    Initial 5 commit of satya prakash

--- db/migrate/20240519135646_add_employee_id_to_user.rb
+++ db/migrate/20240519135646_add_employee_id_to_user.rb
@@ -1,7 +1,6 @@
 class AddEmployeeIdToUser < ActiveRecord::Migration[7.0]
   def change
     add_column :users, :employee_id, :integer
-    add_column :users, :role, :string
     add_foreign_key :users, :employees, column: :employee_id
   end
 end
